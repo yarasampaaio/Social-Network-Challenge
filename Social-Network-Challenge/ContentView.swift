a150fc56fa49c359f6c49ee741fb25274b306a75
yarasampaaio <yaravitoria16@gmail.com>
2022-08-10 09:24:35 -0300

    Initial Commit

---
+++ Social-Network-Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Social-Network-Challenge
+//
+//  Created by Yara Vitoria on 10/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Social-Network-Challenge/Social_Network_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_Network_ChallengeApp.swift
+//  Social-Network-Challenge
+//
+//  Created by Yara Vitoria on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Social_Network_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
