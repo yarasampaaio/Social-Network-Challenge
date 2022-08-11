3d9a97750c0532bdb98aad9e7e116c62e9fbf191
yarasampaaio <yaravitoria16@gmail.com>
2022-08-11 09:12:44 -0300

    second commit

--- Social-Network-Challenge/ContentView.swift
+++ Social-Network-Challenge/ContentView.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var users = [User]()
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView{
+            List(users, id: \.id) { users in
+                VStack(alignment: .leading){
+                    Text(users.name)
+                        .font(.headline)
+                    Text(users.email)
+                    font(.body)
+                }
+            }
+            .navigationTitle("Cadastrados")
+            .task {
+                await getUsers()
+            }
+        }
+    }
+    
+    func getUsers() async {
+        //criando url
+        guard let url = URL(string: "http://adaspace.local/users") else
+        {
+            print("Nao ta funcionando")
+            return
+        }
+        //buscando dados da url em questao
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            //decode that data
+            if let decodeResponde = try?JSONDecoder().decode([User].self, from: data) {
+                users = decodeResponde
+            }
+        } catch {
+            print("this data isn't valid")
+        }
     }
 }
 
---
+++ Social-Network-Challenge/Model/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  Social-Network-Challenge
+//
+//  Created by Yara Vitoria on 11/08/22.
+//
+
+import Foundation
+
+struct User: Codable {
+    var id: String
+    var name: String
+    var email: String
+}
